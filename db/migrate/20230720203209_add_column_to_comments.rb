4eea9a2fd29b3e07179d29172601fc790f67a5b2
TETSU-A7240 <134199699+TETSU-A7240@users.noreply.github.com>
2023-07-23 19:21:32 +0900

    add_column_to_comments.rbの削除
    デプロイにおけるエラーに対しての対処

--- db/migrate/20230720203209_add_column_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToComments < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :comments, :prototype, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_20_203209) do
+ActiveRecord::Schema.define(version: 2023_07_20_180730) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 2023_07_20_203209) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
 end
