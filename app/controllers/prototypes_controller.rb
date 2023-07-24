2f4e716048b1b432a2609b9c477714b9a44371e6
TETSU-A7240 <134199699+TETSU-A7240@users.noreply.github.com>
2023-07-24 19:15:16 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show, :new, :create]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy, :show]
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   
   def index
     @prototypes = Prototype.includes(:user)
